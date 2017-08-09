82bfcc0619aa97489c7c9710affe0869e151259b
Rongpu Zhou <rongpu.zhou@gmail.com>
2017-08-09 15:25:39 -0400

    add KS, CvM and AD statistics

--- metric_scripts/individual_metrics.py
+++ metric_scripts/individual_metrics.py
@@ -1,7 +1,9 @@
+from __future__ import division
 import numpy as np
 import qp
 import matplotlib.pyplot as plt
-
+from scipy import stats
+import skgof
 
 class EvaluateMetric(object):
     def __init__(self,ensemble_obj,truths):
@@ -22,7 +24,7 @@ class EvaluateMetric(object):
         
 
     def PIT(self,using='gridded',dx=0.0001):
-         """ computes the Probability Integral Transform (PIT), described in 
+        """ computes the Probability Integral Transform (PIT), described in 
         Tanaka et al. 2017(ArXiv 1704.05988), which is the integral of the 
         PDF from 0 to zref.
         Parameters:
@@ -35,13 +37,13 @@ class EvaluateMetric(object):
         ndarray
              The values of the PIT for each ensemble object
         """
-         if len(self.truths) != self.ensemble_obj.n_pdfs:
-             print 'Warning: number of zref values not equal to number of ensemble objects'
-             return
-         n = self.ensemble_obj.n_pdfs
-         pitlimits = np.zeros([n,2])
-         pitlimits[:,1] = self.truths
-         return self.ensemble_obj.integrate(limits=pitlimits,using=using,dx=dx)
+        if len(self.truths) != self.ensemble_obj.n_pdfs:
+            print 'Warning: number of zref values not equal to number of ensemble objects'
+            return
+        n = self.ensemble_obj.n_pdfs
+        pitlimits = np.zeros([n,2])
+        pitlimits[:,1] = self.truths
+        return self.ensemble_obj.integrate(limits=pitlimits,using=using,dx=dx)
 
     def QQplot(self,using,dx=0.0001,Nquants=101):
         """Quantile quantile plot for the ensemble using the PIT values, 
@@ -70,3 +72,67 @@ class EvaluateMetric(object):
         plt.legend()
         plt.savefig("QQplot.jpg")
         return
+
+    def KS(self, using, dx=0.0001):
+        """
+        Compute the Kolmogorov-Smirnov statistic and p-value for the PIT 
+        values by comparing with a uniform distribution between 0 and 1. 
+        Parameters:
+        -----------
+        using: string
+            which parameterization to evaluate
+        dx: float
+            step size for integral
+        Returns:
+        --------
+        KS statistic and pvalue
+
+        """
+        pits = np.array(self.PIT(using=using,dx=dx))
+        ks_result = skgof.ks_test(pits, stats.uniform())
+        return ks_result.statistic, ks_result.pvalue
+
+    def CvM(self, using, dx=0.0001):
+        """
+        Compute the Cramer-von Mises statistic and p-value for the PIT values
+        by comparing with a uniform distribution between 0 and 1. 
+        Parameters:
+        -----------
+        using: string
+            which parameterization to evaluate
+        dx: float
+            step size for integral
+        Returns:
+        --------
+        CvM statistic and pvalue
+
+        """
+        pits = np.array(self.PIT(using=using,dx=dx))
+        cvm_result = skgof.cvm_test(pits, stats.uniform())
+        return cvm_result.statistic, cvm_result.pvalue
+
+    def AD(self, using, dx=0.0001, vmin=0.005, vmax=0.995):
+        """
+        Compute the Anderson-Darling statistic and p-value for the PIT 
+        values by comparing with a uniform distribution between 0 and 1. 
+        
+        Since the statistic diverges at 0 and 1, PIT values too close to
+        0 or 1 are discarded. 
+
+        Parameters:
+        -----------
+        using: string
+            which parameterization to evaluate
+        dx: float
+            step size for integral
+        vmin, vmax: floats
+            PIT values outside this range are discarded
+        Returns:
+        --------
+        AD statistic and pvalue
+
+        """
+        pits = np.array(self.PIT(using=using,dx=dx))
+        mask = (pits>vmin) & (pits<vmax)
+        ad_result = skgof.ad_test(pits[mask], stats.uniform())
+        return ad_result.statistic, ad_result.pvalue
