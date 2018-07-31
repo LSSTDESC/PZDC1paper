4cc0b60068f0d87d18cb4218a9f059631eb669e6
Samuel Schmidt <schmidt9@cori05.nersc.gov>
2018-07-31 16:57:30 -0700

    update scripts

--- plotting_scripts/plot_nzsums.py
+++ plotting_scripts/plot_nzsums.py
@@ -1,14 +1,17 @@
-#file to read in all the point pz data
-#Oct 12, 2017
+#Make plot of all N(z) sums including Null aka TrainZ
+#July 2018
 import sys,os
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def main(argv):
     basepathpart = "./TESTDC1"
-    codes = ("ANNZ2","BPZ","EAZY","FLEXZ","GPZ","METAPHOR","SKYNET","TPZ")
-    labels = ("ANNz2","BPZ","EAZY","FlexZBoost","GPz","METAPhoR","SkyNet","TPZ")
+    codes = ("ANNZ2","BPZ","DELIGHT","EAZY","FLEXZ","GPZ","LEPHARE","METAPHOR","NN","SKYNET","TPZ","NULL2")
+    labels = ("ANNz2","BPZ","Delight","EAZY","FlexZBoost","GPz","LePhare","METAPhoR","CMNN","SkyNet","TPZ","TrainZ")
     labeldict = dict(zip(codes,labels))
     
     nzvectorfile = "NZPLOT_vectors.out"
@@ -33,24 +36,24 @@ def main(argv):
 
 
   
-    numrows = 2
+    numrows = 3
     numcols = 4
 
 
-    fig,axes = plt.subplots(numcodes, sharex=True, sharey=True, figsize=(12,6))
-    fig.subplots_adjust(hspace=0.0)
-    fig.subplots_adjust(wspace=0.0)
+    fig,axes = plt.subplots(numcodes, sharex=True, sharey=True, figsize=(12,9))
+    fig.subplots_adjust(hspace=0.025, wspace=0.055)
+    sns.set()
     
     for i in range(numcodes):
         ax = plt.subplot(numrows,numcols,i+1)
-        plt.plot(z_array,szvals[i],c='b',linestyle='-',linewidth=2,label="zspec KDE sum")
+        plt.plot(z_array,szvals[i],c='b',linestyle='-',linewidth=2,label="zspec KDE sum", alpha=0.85)
         tmplabel = "%s N(z) sum"%(labels[i])
-        plt.plot(z_array,pzvals[i],color='r',linestyle='-',linewidth=2,label=tmplabel)
-      
+        plt.plot(z_array,pzvals[i],color='r',linestyle='-',linewidth=2,label=tmplabel, alpha=0.85)
+
         ax.set_xlim([0.0,1.99])
         ax.set_ylim([0.0,1.99])
-        ax.legend(loc="upper left",fontsize=12)
-        if (i<(numcodes-5)):
+        ax.legend(loc="upper left",fontsize=11)
+        if (i<(numcodes-4)):
             ax.set_xticklabels([])
         else:
             ax.set_xlabel("redshift",fontsize=18)
@@ -59,8 +62,11 @@ def main(argv):
         else:
             ax.set_yticklabels([])
 
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=12)
+
    
-    plt.savefig("NZsumplot_10codes.jpg",format='jpg')
+    plt.savefig("NZsumplot_12codes.jpg",format='jpg')
     plt.show()
 
 if __name__=="__main__":
